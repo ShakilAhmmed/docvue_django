7505b4682730ca8a98cd9af6c3f44f98b3824ee6
Shakil Ahmmed <shakilfci461@gmail.com>
2019-06-19 01:23:46 +0600

    Folder

--- document_management_src/myfolder_section/models.py
+++ document_management_src/myfolder_section/models.py
@@ -18,8 +18,27 @@ class MyProjectsModel(models.Model):
     def soft_delete(self):
         self.is_deleted = True
         self.deleted_at = timezone.now()
-        print("ok")
         self.save()
 
     class Meta:
         db_table = "my_projects"
+
+
+class Folder(models.Model):
+    folder_name = models.CharField(max_length=120, unique=True, validators=[clean_project_name])
+    folder_description = models.TextField()
+    parent_folder = models.IntegerField(default=0)
+    created_by = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+    class Meta:
+        db_table = "my_folder"
+
